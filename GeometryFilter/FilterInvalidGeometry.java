18ca066adc682f368a0c0ce0d4b76be3a2c9505b
Jan Nidzwetzki <jnidzwetzki@gmx.de>
2016-10-11 13:20:45 +0200

    Added GeometryFilter

---
+++ GeometryFilter/FilterInvalidGeometry.java
@@ -0,0 +1,60 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+import com.esri.core.geometry.GeometryException;
+import com.esri.core.geometry.ogc.OGCGeometry;
+
+
+public class FilterInvalidGeometry {
+
+	public static void main(final String[] args) throws IOException {
+		
+		if(args.length != 2) {
+			System.err.println("Usage: FilterInvalidGeometry <Input-File> <Output-File>");
+			System.exit(-1);
+		}
+		
+		final File inputFile = new File(args[0]);
+		final File outputFile = new File(args[1]);
+		
+		if(! inputFile.exists()) {
+			System.err.println("Unable to open: " + inputFile);
+			System.exit(-1);
+		}
+		
+		if(outputFile.exists()) {
+			System.err.println("Output file already exists: " + outputFile);
+			System.exit(-1);
+		}
+		
+		final BufferedReader fileReader = new BufferedReader(new FileReader(inputFile));
+		final BufferedWriter output = new BufferedWriter(new FileWriter(outputFile));
+		
+		String line = null;
+		while((line = fileReader.readLine()) != null) {
+			final String[] fields = line.split("\t");
+			
+			if(fields.length < 2) {
+				System.err.println("Unable to parse: " + line + " splits " + fields.length);
+			} else {
+				try {
+					final String formatedString = fields[0].replace("\"", "");
+					final OGCGeometry geom = OGCGeometry.fromText(formatedString);
+					geom.asBinary();
+					
+					output.write(line);
+					output.write("\n");
+				} catch(Exception e) {
+					System.err.println("Exception: Unable to parse: " + fields[0]);
+				}
+			}
+		}
+		
+		fileReader.close();
+		output.close();
+	}
+}
